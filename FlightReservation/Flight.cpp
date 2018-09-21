bdbaa3e973f24fa6878ce1f77b36dd756976c320
Desireé Mayon <34898589+vonneMayon@users.noreply.github.com>
2018-09-21 00:23:30 -0700

    add Flight.cpp

---
+++ FlightReservation/Flight.cpp
@@ -0,0 +1,19 @@
+#include "Flight.h"
+
+using namespace std;
+
+namespace FlightReservation {
+	Flight::Flight(const std::string& number)
+		: mNumber(number) {}
+
+
+	void Flight::setNumber(const string& number)
+	{
+		mNumber = number;
+	}
+	
+	const string& Flight::getNumber() const
+	{
+		return mNumber;
+	}
+}
