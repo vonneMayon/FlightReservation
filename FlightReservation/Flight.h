37943c702fee5dee1bff3b00951e9354cfcf90d5
Desireé Mayon <34898589+vonneMayon@users.noreply.github.com>
2018-09-28 22:03:46 -0700

    adding new flight branch/ details

--- FlightReservation/Flight.h
+++ FlightReservation/Flight.h
@@ -7,10 +7,17 @@ namespace FlightReservation {
 	public:
 		Flight() = default;
 		Flight(const std::string& number);
+		
 
 		//Getters and setters
 		void setNumber(const std::string& number);
 		const std::string& getNumber() const;
+		void setAirline(const std::string& airline);
+		const std::string& getAirline() const;
+
 	private:
-		std::string mNumber;};
+		std::string mNumber;
+		std::string mAirline;
+		
+	};
 }
