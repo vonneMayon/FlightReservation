773d72e11cce8d2e4ebb74076e313f9a4676175f
bethanyzigler <34946388+bethanyzigler@users.noreply.github.com>
2018-09-28 22:49:59 -0700

    Added DOB member field

--- FlightReservation/Passenger.cpp
+++ FlightReservation/Passenger.cpp
@@ -5,8 +5,9 @@
 using namespace std;
 
 namespace FlightReservation {
-	Passenger::Passenger(const std::string& firstName)
-		: mFirstName(firstName) {}
+	Passenger::Passenger(const std::string& firstName, const std::string& birthDate)
+		: mFirstName(firstName), mBirthDate(birthDate){}
+
 
 	void Passenger::setFirstName(const string& firstName)
 	{
@@ -16,5 +17,15 @@ namespace FlightReservation {
 	const string& Passenger::getFirstName() const
 	{
 		return mFirstName;
+	}
+
+	void Passenger::setBirthDate(const string& birthDate)
+	{
+		mBirthDate = birthDate;
+	}
+
+	const string& Passenger::getBirthDate() const
+	{
+		return mBirthDate;
 	}
 }
--- FlightReservation/Passenger.h
+++ FlightReservation/Passenger.h
@@ -6,14 +6,17 @@ namespace FlightReservation {
 	{
 	public:
 		Passenger() = default;
-		Passenger(const std::string& firstName);
+		Passenger(const std::string& firstName, const std::string& birthDate);
 
 		//Getters and Setters
 		void setFirstName(const std::string& firstName);
 		const std::string& getFirstName() const;
+		void setBirthDate(const std::string& birthDate);
+		const std::string& getBirthDate() const;
 
 	private:
 		std::string mFirstName;
+		std::string mBirthDate;
 	};
 }
 
